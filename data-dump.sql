33f37d19046a2494b9cbddf17b02a3b5f6fd1951
21DP4MGalk <14DPMGalkins@rvt.lv>
2024-05-07 04:15:42 +0300

    Finishing touches for now

--- data-dump.sql
+++ data-dump.sql
@@ -1,27 +1,23 @@
--- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
---
--- Host: localhost    Database: MusicProject
--- ------------------------------------------------------
--- Server version	8.0.36-0ubuntu0.20.04.1
+CREATE TABLE `users` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `type` enum('temp','registered','admin') DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+)
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `instruments`
---
+CREATE TABLE `pieces` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(20) DEFAULT NULL,
+  `file` mediumblob,
+  `userID` int NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `userID` (`userID`),
+  CONSTRAINT `pieces_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`ID`)
+)
 
-DROP TABLE IF EXISTS `instruments`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `instruments` (
   `ID` int NOT NULL AUTO_INCREMENT,
   `name` varchar(20) DEFAULT NULL,
@@ -31,78 +27,6 @@ CREATE TABLE `instruments` (
   PRIMARY KEY (`ID`),
   KEY `pieceID` (`pieceID`),
   CONSTRAINT `instruments_ibfk_1` FOREIGN KEY (`pieceID`) REFERENCES `pieces` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) 
 
---
--- Dumping data for table `instruments`
---
 
-LOCK TABLES `instruments` WRITE;
-/*!40000 ALTER TABLE `instruments` DISABLE KEYS */;
-/*!40000 ALTER TABLE `instruments` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `pieces`
---
-
-DROP TABLE IF EXISTS `pieces`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `pieces` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(20) DEFAULT NULL,
-  `file` mediumblob,
-  `userID` int NOT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `userID` (`userID`),
-  CONSTRAINT `pieces_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `pieces`
---
-
-LOCK TABLES `pieces` WRITE;
-/*!40000 ALTER TABLE `pieces` DISABLE KEYS */;
-/*!40000 ALTER TABLE `pieces` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(20) NOT NULL,
-  `password` varchar(128) NOT NULL,
-  `type` enum('temp','registered','admin') DEFAULT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Martin','faggot','admin');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-05-03 21:50:43

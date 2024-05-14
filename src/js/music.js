e336a58f1b7757b559adaa2a762f7f1fbc91b6bc
21DP4MGalk <14DPMGalkins@rvt.lv>
2024-05-14 05:37:56 +0300

    Rewrote note placement to use canvas

--- src/js/music.js
+++ src/js/music.js
@@ -1,14 +1,17 @@
-const MainContext = new AudioContext();
+const AContext = new AudioContext();
 var temperament = [261.6255653005985, 277.182630976872, 293.66476791740746, 311.1269837220808, 329.62755691286986, 349.2282314330038, 369.99442271163434, 391.99543598174927, 415.3046975799451, 440, 466.1637615180899, 493.8833012561241, 523.2511306011974];
-var temperamentList = [[], []]; // NOTE: include 12tet, pythagorean, and some medieval ones, aswell as custom, of course.
+//var temperamentList = [[], []]; // NOTE: include 12tet, pythagorean, and some medieval ones, aswell as custom, of course.
 var concertA = 440;
+var notationCan;
+var notationCtx;
+
 
 function getPitchGivenA(desiredPitchNum, referenceANum = 69, referenceA = 440){		
 	// desiredPitchNum is the number assigned to the pitch we want to generate in a scale, for example how Middle C in MIDI format is 60
 	// referenceANum is the number assigned to concert A in the same context, reference A defines concert A. 
 	//All values should be INT
 	if(isNaN(desiredPitchNum) || isNaN(referenceANum) || isNaN(referenceA)){
-		throw new TypeError("One or more of of the parameters used for generating a pitched turned out not to be numbers, but rapscallions in trench coats!");
+		throw new TypeError("One or more of of the parameters used for generating a pitches turned out not to be numbers, but rapscallions in trench coats!");
 	}
 
 	return (referenceA * (Math.pow( Math.pow(2, 1/12), (desiredPitchNum - referenceANum) ) ));
@@ -27,9 +30,10 @@ function getChromatic12TET(referenceA){
 }
 
 class Note{
-	constructor(pitch, duration){ //both must be numeric
+	constructor(pitch, duration, X = 0){ //all must be numeric
 		this.pitch = pitch;
 		this.duration = duration;
+		this.X = X;
 	}
 }
 
@@ -42,20 +46,20 @@ class MusicTextReader{
 	
 	playNote(note, delay, attackTime = 0.1, releaseTime = 3){
 
-		const oscillator = MainContext.createOscillator();
+		const oscillator = AContext.createOscillator();
 		oscillator.type = 'sine';
 		oscillator.frequency.value = note.pitch;
 
-		const gainModule = new GainNode(MainContext);
-		gainModule.gain.cancelScheduledValues( MainContext.currentTime + delay);
-		gainModule.gain.setValueAtTime(0, MainContext.currentTime + delay);
-		gainModule.gain.linearRampToValueAtTime(1, MainContext.currentTime + delay + attackTime);
-		gainModule.gain.linearRampToValueAtTime(0, MainContext.currentTime + delay + releaseTime);
+		const gainModule = new GainNode(AContext);
+		gainModule.gain.cancelScheduledValues( AContext.currentTime + delay);
+		gainModule.gain.setValueAtTime(0, AContext.currentTime + delay);
+		gainModule.gain.linearRampToValueAtTime(1, AContext.currentTime + delay + attackTime);
+		gainModule.gain.linearRampToValueAtTime(0, AContext.currentTime + delay + releaseTime);
 
 
-		oscillator.connect(gainModule).connect(MainContext.destination);
- 	   	oscillator.start( MainContext.currentTime + delay);
-		oscillator.stop( MainContext.currentTime + delay + (note.duration * this.bottom_time * (60/this.bpm) ) );
+		oscillator.connect(gainModule).connect(AContext.destination);
+ 	   	oscillator.start( AContext.currentTime + delay);
+		oscillator.stop( AContext.currentTime + delay + (note.duration * this.bottom_time * (60/this.bpm) ) );
 	}
 
 	playString(){
@@ -109,18 +113,86 @@ class MusicTextReader{
 }
 
 class GrandStaff{
-	constructor(bpm, bottomTime, topTime, smallestUnit, song = [[]]){
-		this.bpm = bpm;
-		this.bottomTime = bottomTime;
-		this.topTime = topTime;
-		this.smallestUnit = smallestUnit;
-		generateEmptyStaff();
+	constructor(){
+		this.noteArr = [
+			[], [], [], [], [], 
+			[], [], [], [], [], 
+			[], [], [], [], [], 
+			[], [], [], [], [],
+			[], [], [], []
+		]	
+	}
+	
+	checkAvailable(Y, X){
+		
+	}
+	
+	addNote(Y, X, duration){
+		
+	}
+	
+	drawStaff(){
+
+	}
+}
+
+function initCtx(){
+	notationCan = document.getElementById("notation");
+	notationCtx = notationCan.getContext("2d");
+	notationCtx.font = "50px LelandMusic";
+
+	//notationCan.width = window.screen.width * 0.95;
+	//notationCan.height = window.screen.height;
+	
+	var i;
+	
+	for(i = 0; i < 5; i++){
+		notationCtx.moveTo(0, 100 + i * 15);
+		notationCtx.lineTo(notationCan.width, 100 + i * 15);
+		notationCtx.stroke();
+		
+	}
+	
+	for(i = 0; i < 5; i++){
+		notationCtx.moveTo(0, 190 + i * 15);
+		notationCtx.lineTo(notationCan.width, 190 + i * 15);
+		notationCtx.stroke();
+
 	}
 
-	generateEmptyStaff(){
-		emptynote = new Note(0, this.smallestUnit);
-		//for(i = 0; i<)
+	notationCtx.stroke();
+	notationCtx.fillText(String.fromCharCode(parseInt('E0A2', 16)), -100, -100);
+
+
+}
+
+function interpretClick(){
+	
+	var rect = notationCan.getBoundingClientRect();
+	var mouseX = event.clientX - rect.left;
+	var mouseY = event.clientY - rect.top;
+	console.log(mouseX);
+	console.log(mouseY);
+	
+
+
+	var selectedNote = document.getElementById('noteSelect');
+	selectedNote = selectedNote.options[selectedNote.selectedIndex].value;
+
+	mouseY = mouseY - ((mouseY-2) % 7.5);
+	mouseX = mouseX - 7;
+
+	if(mouseY < 80 || mouseY > 264.5){
+		return;
 	}
+	
+	var lineFromTop = Math.floor((mouseY-80)/7.5);
+	console.log(lineFromTop)
+	console.log(mouseY)
+
+	
+	//grandstaff.checkavailable(mouseY, mouseX);
+	notationCtx.fillText(String.fromCharCode(parseInt(selectedNote, 16)), mouseX, mouseY);	
 }
 
 function testStringPlayer(){
@@ -128,444 +200,11 @@ function testStringPlayer(){
 	reader.playString();
 }
 
-function addNoteToStaff(){}
-
-function refreshNotePosition(id){
-	var staffLine = document.getElementById(id);
-	var notes = document.getElementById(id + 'n');
-	notes.style.top = ((staffLine.getBoundingClientRect().top + window.scrollY)) - 145 + 'px';	
-}
-
-function placeNoteStrings(){
-	allLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
-	allNums = ['1', '2', '3', '4'];
-	for(i = 0; i < allLetters.length; i++){
-		for(j = 0; j < allNums.length; j++){
-			if( (i == 3 || i == 2) && (j == 0) || ((i == 0 || i == 1) && j==3 ) ){
-				continue;
-			}
-			else{
-				refreshNotePosition(allLetters[i] + allNums[j]);
-			}
-		}
-	}
-}
 
 function getPointedElement(){ 
 	var q = document.querySelectorAll(":hover"); 
 	return q[q.length-1]; 
 }
 
-function interpretClick(){
-	var staffLine = getPointedElement();
-	console.log(event.clientX);
-	var charClicked = Math.round(event.clientX/34);
 
-	if(staffLine.id != "notation"){
-		var noteString = document.getElementById(staffLine.id + 'n');
-		var selectedNote = document.getElementById('noteSelect');
-		selectedNote = selectedNote.options[selectedNote.selectedIndex].value;
-		
-
-		if(noteString.innerHTML.length >= charClicked && (noteString.innerHTML.charAt(charClicked) != ' ') ){
-			noteString.innerHTML = noteString.innerHTML.substring(0, charClicked-1) + ' ' + noteString.innerHTML.substring(charClicked+1, noteString.innerHTML.length);
-			return;
-		}
-
-		for(i = noteString.innerHTML.length; i <= charClicked; i++){
-			noteString.innerHTML += ' ';
-		}
-
-		//noteString.value += selectedNote + ' ';
-		noteString.innerHTML = noteString.innerHTML.substring(0, charClicked) + selectedNote + noteString.innerHTML.substring(charClicked, noteString.innerHTML.length);
-		
-		console.log(document.getElementById(staffLine.id + 'n').innerHTML);
-	}
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*			CAPYBARA JUMPSCARE
- 
-⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢛⠃⣔⢦⠀⢈⢡⠑⣆⢂⡀⡘⠋⡀⣄⣒⠐⡄⠆⠀⢀⣤⣒⠤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⢤⣀⡘⢾⣭⢘⡾⣿⣾⣿⣿⣷⣿⣿⣯⣅⡷⠐⠐⢸⠉⠸⡷⢾⡷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣄⡂⢩⢶⡳⣶⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣟⣴⣽⡆⢏⣍⡘⣺⣽⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣌⠙⢹⣶⣒⡹⣯⡽⣿⣬⢷⣿⣿⣿⣿⣿⣿⣏⣿⣿⣭⡏⣬⣿⡼⠉⠉⠩⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠞⠁⠈⠠⢉⡷⢧⠘⣇⣹⣏⢻⣿⣷⣞⣯⣷⣿⣽⡿⢿⡿⣽⠻⣼⣏⡀⠀⠀⣈⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⠶⣶⣴⣤⡴⠿⠏⢸⠝⣺⣯⣿⣿⡿⡿⢫⡽⣿⣿⢿⣿⣼⡷⣛⢿⣻⣵⣿⣿⡿⠛⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠟⠁⠸⣓⡺⣾⣷⣿⡟⣟⠸⣇⣋⡱⢏⡿⣿⣿⣿⣿⢞⣿⢧⡹⣿⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣛⣀⠀⢀⠃⠸⢇⡸⣿⣿⠣⠸⠃⠀⠀⠸⠛⠤⣛⣿⡿⣟⣻⣧⣟⣠⡜⣻⣇⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠦⠤⡠⠒⣶⠋⣙⢢⡳⠮⣿⣟⠀⠀⠀⠀⠀⠀⠀⠰⡹⢿⣱⠿⢟⣵⣉⢿⣧⡴⣬⠈⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠒⠂⠙⡁⣀⠴⠂⠅⠬⠉⠈⠀⠐⢒⠀⠀⠀⠀⢀⡀⠠⠀⡀⠉⡁⠈⠉⠠⢞⡚⠽⣻⣬⢷⡀⠽⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠤⠤⢀⢔⡨⢎⠁⠀⠀⠀⢀⣤⣭⣤⣰⣘⡴⣋⣶⣰⣧⣾⣿⣿⣦⡀⡼⢵⣯⣛⣩⡝⣿⣵⣎⣿⡷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢢⡾⣂⢋⡰⠇⣫⣜⣫⡄⠀⠀⠀⠸⠿⣿⡿⡙⣿⣷⣿⣿⣿⢿⣿⣿⣿⢏⡃⠀⢤⣼⣽⣽⣯⣽⣻⣭⣟⣿⣿⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣐⣷⡚⡱⠓⢶⣲⣗⣪⢷⡖⡢⠀⠀⡜⣃⠍⠀⠘⢻⣿⣿⣿⡯⢂⢩⣿⡥⢿⡀⢲⣺⣿⣿⣿⣽⣿⣽⣿⣿⢻⣯⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀
-⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣍⣷⠟⢷⣯⠯⣱⣭⠟⡿⠓⠂⠀⠀⠀⠰⠛⠁⣰⢦⢹⣿⣳⢚⡥⠂⡟⠙⠟⠀⠀⣻⣿⢿⣾⣿⣿⣿⢿⣿⣿⣿⣿⣿⣦⠀⡄⠀⠀⠀⠀⠀
-⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡯⢿⣿⣟⢟⣽⠿⠛⢣⣴⠣⠀⠀⠀⠀⠀⠄⠒⠉⢣⣿⡟⣶⣧⡿⣾⣝⡋⠹⠦⠀⠀⠶⣿⣿⣯⣻⣿⣿⣿⣿⣿⣿⣿⣯⢿⣮⡁⠀⠀⠀⠀⠀
-⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⢨⣶⣿⣟⣡⣬⡵⠖⡽⢇⡐⠭⠀⠀⠀⠀⠀⠀⠠⠶⣨⣷⣾⣷⣿⡽⡖⠠⠜⢀⠆⠀⠢⠖⢿⣵⣎⠿⢯⣿⣾⣿⡿⣿⣿⣿⣳⣼⡧⣄⠀⠀⠀⠀
-⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⢋⣿⣷⣛⠴⣫⠄⣋⢸⡅⠄⠐⠀⠀⠀⠀⠀⠀⠀⠀⣱⠼⣿⣿⣿⡷⢋⡤⣄⠦⡙⢈⠑⠤⡀⠒⠿⣽⢶⣯⣿⣿⣽⣿⣿⣷⣿⣼⣿⡀⠉⠀⠀⠀
-⡇⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣊⡱⢎⣷⡽⠲⣯⣾⡷⠉⣒⠢⣢⡴⠀⠀⠀⠀⠀⠐⡀⠤⢻⣿⡟⠉⠂⠀⠈⠑⠡⣀⡑⢶⣬⣐⣾⣭⣦⣝⣭⣛⠿⣿⣽⡿⢿⣻⣿⣭⠁⠀⠀⠀
-⡇⠀⠀⠀⠀⠀⠀⠀⠠⢴⡸⠋⢀⡿⢃⠀⢚⣩⣟⡴⠛⣥⠞⢋⠀⠄⠀⠀⠀⣀⠠⢃⢨⣳⣿⣽⣴⣆⣲⣍⣠⣁⠦⣘⣀⡻⣝⣾⣭⣽⣿⡷⢿⣿⣭⣛⣽⢫⢽⣿⡳⠄⠀⠀⠀
-⡇⠀⠀⠀⠀⠀⠀⠀⢚⡵⢀⡁⠜⠢⣙⠤⡪⡡⣟⠉⣀⣠⡾⢏⠂⢊⣀⠔⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣯⣻⣷⣿⣧⣵⣿⣿⣯⢟⣿⢦⡟⠫⣤⣛⠾⣯⣟⣦⠀⠀⠀
-⡇⠀⠀⠀⠀⠀⠀⠰⠈⢢⠴⠆⠠⠄⠀⠈⠒⣋⡴⢚⢟⣥⣆⡮⠸⠙⢋⠆⢻⡻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣿⣧⣋⢿⣥⢻⣿⢿⣿⣿⣞⣛⢏⡙⣦⣈⠹⣿⣿⣿⡡⠀⠀⠀
-⡇⠀⠀⠀⠀⠀⠀⠀⡄⠀⠂⢠⠀⠀⠀⠀⠐⠈⠘⣵⢫⣿⡏⡄⠁⣤⣾⢰⡎⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⡟⣦⣽⣾⣽⣿⣿⣽⣮⠓⡅⡌⢪⠋⣷⣦⣵⡟⣷⠁⠀⠀
-⡇⠀⠀⠀⠀⠀⠠⣺⡉⡄⠒⠡⠤⡀⠀⠀⠀⠀⠠⠇⣹⡥⠞⠃⣼⣿⢧⣿⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣿⢿⣾⢽⡋⢮⣍⠢⣍⠒⠬⣯⣝⣾⣿⣶⢃⠀⠀
-⡇⠀⠀⠀⠀⠀⢰⡁⠀⢀⡴⡶⠍⠐⠀⠀⠀⠀⠀⡼⠋⡀⠀⣴⣻⣿⢹⣷⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣯⢿⣿⣻⣦⣽⣦⣬⣑⣦⣠⣤⣼⣿⣿⢯⣂⠀
-⡇⠀⠀⠀⠀⠀⣠⢖⡬⠍⠶⠋⠠⣈⢀⠠⠀⡲⠏⠂⠔⣣⢜⣿⣽⢋⣾⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣞⣮⢾⣆⡙⢳⣟⣻⣿⣿⣿⡷⢶⡄
-⡇⠀⠀⠀⠀⡘⠥⠷⡊⠥⢀⡴⠛⣐⣊⣰⢟⡵⢚⠾⠙⣫⣟⣋⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣻⣝⣛⣯⡗⢯⡹⢾⡿⣿⣿⣽⣿⣽⣛⠾
-⡇⠀⠀⠄⠈⣠⡤⢊⠖⡹⠋⣠⡎⠽⢋⢕⡬⢴⣊⡾⢉⠟⢐⣣⣲⣼⣿⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣿⡿⢯⣿⣹⣿⡽⠿⣶⣉⡞⣿⣽⡾⣟⣷
-⣧⡀⠀⢀⠆⣵⠊⠱⠋⢟⢵⠋⡒⠧⣿⠏⡺⣽⠼⢟⠵⣮⢟⠻⣝⠿⢶⣻⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⡿⢻⣟⢷⣾⣛⣿⣽⣿⣿⣟⣾
-⣿⣷⣤⣀⣚⠕⠫⡥⢀⠖⣀⡞⡹⢪⠡⠞⢟⡣⠼⢖⣫⡽⣋⡴⣽⠻⣟⣽⡾⣳⡿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣶⣹⣿⣬⣝⡺⢿⣾⢻⣿⣽⣾⣿
-*/
+grandestStaff = new GrandStaff();
